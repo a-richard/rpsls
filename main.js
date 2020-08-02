ec33759a20b5e315e78cdd086b28f2fdf05bafd3
Aurelien Richard <a.richard.dev@gmail.com>
2020-08-02 15:06:01 +0200

    Complete both middle sections. Add modal box for rules and JS functionalities to open and close it.

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+// Rules modal box
+let rulesBox = document.querySelector('.rules-background');
+let closeButton = document.querySelector('.close-icon');
+let rulesButtons = document.querySelectorAll('.rules-button');
 
+rulesButtons.forEach(function (button) {
+  button.onclick = function () {
+    rulesBox.style.display = 'block';
+  };
+});
+
+closeButton.onclick = function () {
+  rulesBox.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === rulesBox) {
+    rulesBox.style.display = 'none';
+  }
+};
